894d0b2c0b57b292478550bcd49a43e1c3d0b85e
Iain le Roux <iain.leroux@entelect.co.za>
2025-08-17 18:04:36 +0200

    Removed code for player

--- state_management.py
+++ state_management.py
@@ -11,32 +11,3 @@ def setup_board():
 # TODO: Update board state based on move played (player, location, piece type)
 def make_move(move: dict[str, str]):
     pass
-
-
-def play_game():
-    print("Starting the game...")
-    reset_game_info()
-
-    print("Game setup...")
-    setup_board()
-
-    while True:
-        next_move = input('Enter next move: ')
-
-        if next_move in ['exit', 'quit']:
-            print("Exiting the game.")
-            break
-
-        make_move()
-
-
-if __name__ == "__main__":
-    cell_list, player_clues = read_info_file()
-
-    if len(argv) > 1 and argv[1] in ['reset', 'setup']:
-        reset_game_info()
-        exit
-    
-    if len(argv) > 1 and argv[1] == 'play':
-        play_game()
-        exit
